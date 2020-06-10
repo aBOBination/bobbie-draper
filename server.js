7d1e40655409a7a58fd9498e2666fdd0994437ca
msyphus <mark.syphus@gmail.com>
2020-06-09 20:48:22 -0600

    created controller

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -22,8 +23,8 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+var routes = require('./controller/menuController');
+app.use(routes);
 
 var syncOptions = { force: false };
 
