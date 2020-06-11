14098a64d5ffe8c93757f0fdf44efe04017fb972
msyphus <mark.syphus@gmail.com>
2020-06-10 23:12:07 -0600

    added controller

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.engine(
 app.set('view engine', 'handlebars');
 
 // Routes
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+var routes = require('./controller/menuController');
+
+app.use(routes);
 
 var syncOptions = { force: false };
 
