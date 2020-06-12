09673c78e6326a244137decd81be4701c7447a9f
Robert Marks <rmarks@nav.com>
2020-06-12 01:06:42 -0600

    split out controller etc

---
+++ controller/apiController.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.get('/api/trucks', function(req, res) {
+  db.trucks.findAll({ include: [db.menu_items] }).then(function(data) {
+    res.json(data);
+  });
+});
+
+router.post('/api/trucks', function(req, res) {
+  db.trucks.create(req.body).then(function(data) {
+    res.json(data);
+  });
+});
+
+router.delete('/api/trucks/:id', function(req, res) {
+  db.trucks.destroy({ where: { id: req.params.id } }).then(function(data) {
+    res.json(data);
+  });
+});
+
+module.exports = router;
